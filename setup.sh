817d56d65b8dabcc274d1d949f88c4617d65976f
AnalyseAnalyseindsigtIndsigt <analyseindsigt@AnalyseIndsigts-iMac.local>
2017-12-22 11:05:16 +0100

    Additions

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
-ssh -i rheltest.pem ec2-user@ec2-18-217-82-69.us-east-2.compute.amazonaws.com
+# AMAZON
+# ssh -i rheltest.pem ec2-user@ec2-18-217-82-69.us-east-2.compute.amazonaws.com
+
+# ssh singularity001@pfacdmlr01.westeurope.cloudapp.azure.com
+
 
 # Create user
-useradd jwaage
+sudo useradd jwaage
 sudo passwd jwaage
+sudo usermod -aG wheel jwaage
+
+sudo useradd mac
+sudo passwd mac
+
+sudo useradd emd
+sudo passwd emd
+
 
 # Install helper packages
 sudo yum -y install emacs
@@ -21,15 +33,15 @@ sudo yum -y install screen
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/texlive-epsf-doc-svn21461.2.7.4-38.el7.noarch.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/texlive-epsf-svn21461.2.7.4-38.el7.noarch.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/texinfo-tex-5.1-4.el7.x86_64.rpm
-sudo yum localinstall texlive-epsf-doc-svn21461.2.7.4-38.el7.noarch.rpm
-sudo yum localinstall texlive-epsf-svn21461.2.7.4-38.el7.noarch.rpm
-sudo yum localinstall texinfo-tex-5.1-4.el7.x86_64.rpm
+sudo yum localinstall -y texlive-epsf-doc-svn21461.2.7.4-38.el7.noarch.rpm
+sudo yum localinstall -y texlive-epsf-svn21461.2.7.4-38.el7.noarch.rpm
+sudo yum localinstall -y texinfo-tex-5.1-4.el7.x86_64.rpm
 
 sudo yum install -y R
 
 # Python
-yum install gcc openssl-devel bzip2-devel
-yum install -y python34.x86_64
+sudo yum install gcc openssl-devel bzip2-devel
+sudo yum install -y python34.x86_64
 
 # Rstudio server
 wget https://download2.rstudio.org/rstudio-server-rhel-1.1.383-x86_64.rpm
@@ -49,3 +61,7 @@ emacs /home/ec2-user/.jupyter/jupyter_notebook_config.py
 
 # Start jupyter
 /opt/anaconda/bin/jupyter notebook --ip 172.31.20.152 --port 8888 --notebook-dir ~
+
+# Firewall
+sudo firewall-cmd --permanent --zone=public --add-port=8787/tcp
+sudo firewall-cmd --reload
